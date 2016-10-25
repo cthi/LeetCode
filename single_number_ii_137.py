0649982097df4ad15a7c0839302bb61e63f13e1b
Chris Thi <chris.c.thi@gmail.com>
2016-10-24 19:29:22 -0700

    Bump

---
+++ single_number_ii_137.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        ht = {}
+        
+        for num in nums:
+            if num in ht:
+                ht[num] += 1
+                
+                if ht[num] > 3:
+                    return num
+            else:
+                ht[num] = 1
+                
+        for key,value in ht.items():
+            if value < 3:
+                return key
+        
