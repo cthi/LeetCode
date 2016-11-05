f3f179f5354e39b94435f499329ec9c9dd9c507e
Chris Thi <chris.c.thi@gmail.com>
2016-11-04 23:28:26 -0700

    Bump

---
+++ add_two_numbers_ii_445.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        res = self.asInt(l1) + self.asInt(l2)
+        
+        ans = None
+        head = None
+        
+        for char in str(res):
+            node = ListNode(int(char))
+            
+            if ans:
+               ans.next = node
+            else:
+                head = node
+                
+            ans = node
+            
+        return head
+        
+        
+    def asInt(self, node):
+        num = ""
+        
+        while node:
+            num += str(node.val)
+            node = node.next
+            
+        return int(num)
+
