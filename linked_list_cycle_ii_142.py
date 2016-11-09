6e09e5c366711ce7fa5b72000495d4d5d80a9dbc
Chris Thi <chris.c.thi@gmail.com>
2016-11-08 22:20:06 -0800

    Bump

---
+++ linked_list_cycle_ii_142.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def detectCycle(self, head):
+        seen = set()
+        
+        while head:
+            if head in seen:
+                return head
+            else:
+                seen.add(head)
+                
+            head = head.next
+            
+        return None
