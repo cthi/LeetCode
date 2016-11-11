4477c97e4c1ac326ef2c5fd46d594552951a6405
Chris Thi <chris.c.thi@gmail.com>
2016-11-10 22:49:47 -0800

    Bump

---
+++ gas_station_134.py
@@ -0,0 +1,32 @@
+ass Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        if sum(cost) > sum(gas):
+            return -1
+        
+        N = len(gas)
+        i = 0
+        seen = 0
+        tank = 0
+        ans = 0
+        
+        while seen < N:
+            tank += gas[i]
+            tank -= cost[i]
+            
+            i += 1
+            seen += 1
+            
+            if tank < 0:
+                tank = 0
+                seen = 0
+                ans = i
+            
+            if i == N:
+                i = 0
+                
+        return ans
