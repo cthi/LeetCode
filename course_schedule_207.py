495568ef4e3ffb4ee4353351e241273c402ec23d
Chris Thi <chris.c.thi@gmail.com>
2016-11-12 00:29:58 -0800

    Bump

---
+++ course_schedule_207.py
@@ -0,0 +1,31 @@
+ass Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        graph = [[] for i in range(numCourses)]
+        
+        for req in prerequisites:
+            graph[req[0]].append(req[1])
+        
+        seen = [False] * numCourses
+        stack = [False] * numCourses
+
+        for course in range(numCourses):
+            if self.hasCycle(graph, course, seen, stack):
+                return False
+        
+        return True
+
+
+    def hasCycle(self, graph, course, seen, stack):
+        if not seen[course]:
+            seen[course] = True
+            stack[course] = True
+    
+            for reqs in graph[course]:
+                if stack[reqs]:
+                    return True
+                if self.hasCycle(graph, reqs, seen, stack):
+                    return True
+            
+            stack[course] = False
+
+        return False 
---
+++ course_schedule_ii_210.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = [[] for i in range(numCourses)]
+        
+        for req in prerequisites:
+            graph[req[0]].append(req[1])
+        
+        seen = [False] * numCourses
+        stack = [False] * numCourses
+        order = []
+
+        for course in range(numCourses):
+            if not self.topsort(graph, course, seen, order, stack):
+                return []
+
+        return order
+
+
+    def topsort(self, graph, course, seen, order, stack):
+        if not seen[course]:
+            seen[course] = True
+            stack[course] = True
+    
+            for req in graph[course]:
+                if stack[req]:
+                    return False
+                if not self.topsort(graph, req, seen, order, stack):
+                    return False
+            
+            stack[course] = False
+            order.append(course)
+            
+        return True
