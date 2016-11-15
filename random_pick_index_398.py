a14b2f7c60b90f099c91e613464621a7dbdcfb4d
Chris Thi <chris.c.thi@gmail.com>
2016-11-14 20:24:52 -0800

    Bump

---
+++ random_pick_index_398.py
@@ -0,0 +1,27 @@
+import random
+
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        :type numsSize: int
+        """
+        self.nums = nums
+
+    def pick(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        index = 0
+        seen = 0
+        
+        for i, num in enumerate(self.nums):
+            if num == target:
+                seen += 1
+                if random.randint(0, seen - 1) == 0:
+                    index = i
+                    
+        return index
+
