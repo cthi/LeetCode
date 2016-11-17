e22ddf390e5ceb1e44fcf88fbde306ec8249f184
Chris Thi <chris.c.thi@gmail.com>
2016-11-16 20:58:35 -0800

    Bump

---
+++ assign_cookies_455.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        count = 0
+        g = sorted(g)
+        s = sorted(s)
+        
+        start = 0
+        
+        for cookie in s:
+            if start == len(g):
+                break
+            
+            if g[start] <= cookie:
+                count += 1
+                start += 1
+                
+        return count
---
+++ frog_jump_403.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def canCross(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: bool
+        """
+        if stones[0] != 0 or stones[1] != 1:
+            return False
+        
+        return self.canCrossHelper(1, 1, stones[-1], set(stones), {})
+        
+        
+    def canCrossHelper(self, curStone, lastJump, goal, stones, memo):
+        if curStone == goal:
+            return True
+        elif (curStone, lastJump) in memo:
+            return memo[(curStone, lastJump)]
+        elif curStone not in stones:
+            return False
+        else:
+            res = self.canCrossHelper(curStone + lastJump + 1, lastJump + 1, goal, stones, memo)
+            res = res or self.canCrossHelper(curStone + lastJump, lastJump, goal, stones, memo)
+            if lastJump != 1:
+                res = res or self.canCrossHelper(curStone + lastJump - 1, lastJump - 1, goal, stones, memo)
+                
+            memo[(curStone, lastJump)] = res
+            return res
