0612721daa43fa5e1b2c01c94aec08333a9a3980
Chris Thi <chris.c.thi@gmail.com>
2016-11-19 21:55:51 -0800

    Bump

---
+++ circular_array_loop_457.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def circularArrayLoop(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        N = len(nums)
+        seen = [False for i in range(N)]
+        
+        for i in range(N):
+            if not seen[i]:
+                seen[i] = True
+                sign = nums[i] > 0
+                start = i
+                cur = i
+                length = 1
+
+                while ((nums[(cur + nums[cur]) % N] > 0) == (nums[start] > 0)):
+                    cur = (cur + nums[cur]) % N
+                    
+                    if length > 1 and cur == start:
+                        return True
+                    elif seen[cur]:
+                        break
+                    
+                    seen[cur] = True
+                    length += 1
+                        
+        return False
