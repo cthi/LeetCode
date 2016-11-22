df9ac1343a8619a7a0cbf6f9cdabe7eb0b3c3c3d
Chris Thi <chris.c.thi@gmail.com>
2016-11-21 22:43:00 -0800

    Bump

---
+++ island_perimeter_463.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+        N = len(grid)
+        M = len(grid[0])
+        perimeter = 0
+        
+        for i in range(N):
+            for j in range(M):
+                if grid[i][j] == 1:
+                    if i + 1 > N - 1 or grid[i + 1][j] == 0:
+                        perimeter += 1
+                    if i - 1 < 0 or grid[i - 1][j] == 0:
+                        perimeter += 1
+                    if j + 1 > M - 1 or grid[i][j + 1] == 0:
+                        perimeter += 1
+                    if j - 1 < 0 or grid[i][j - 1] == 0:
+                        perimeter += 1
+
+        return perimeter
